905298ade64e384ce236600ce5d953b045bdb7ef
abakanina <abakanina@erecruit.com>
2019-04-26 18:17:14 +0300

    adding of new tasks (need to be finished)

---
+++ Task7/Task7/Country.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    public class Country
+    {
+        public string Name;
+        public bool IsTelenorSupported;
+
+        public Country()
+        {
+            Name = "";
+            IsTelenorSupported = false;
+        }
+
+        public Country(string Name)
+        {
+            this.Name = Name;
+        }
+
+
+    }
+}
---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "D:\\AQA\\NewFile.txt";
+            Dictionary<int, Country> countries = new Dictionary<int, Country>();
+            FileDialog();
+            Output();
+
+            void FileDialog()
+            {
+                int i = 1;
+                using (StreamReader stream = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = stream.ReadLine()) != null)
+                    {
+                        countries.Add(i,new Country(line));
+                        i++;
+                    }
+                }
+            }
+
+            void Output()
+            {
+                foreach (KeyValuePair<int, Country> keyValue in countries)
+                {
+                    Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name);
+                }
+            }
+        }
+    }
+}
