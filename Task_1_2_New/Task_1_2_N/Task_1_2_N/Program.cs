61fb7422d7058d558964b46dfe30627e73beefea
Anna Bakanina <aneta333neta@gmail.com>
2019-05-16 17:24:22 +0300

    rewited Task 1 and Task 2

---
+++ Task_1_2_New/Task_1_2_N/Task_1_2_N/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(ConsoleHelper.GetValueToCreateObjectTypeOfCircle());
+            Square square = new Square(ConsoleHelper.GetValueToCreateObjectTypeOfSquare());
+            ConsoleHelper.GetResult(square, circle);
+        }
+    }
+}
---
+++ Task_1_2_New/Task_1_2_N/Task_1_2_N/Square.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2_N
+{
+    class Square
+    {
+        private double _side;
+        public double Side
+        {
+            get
+            {
+                return _side;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _side = value;
+                }
+            }
+        }
+
+        public Square()
+        {
+            Side = 0.0;
+        }
+
+        public Square(double squareSide)
+        {
+            Side = squareSide;
+        }
+
+        public double GetAreaOfSquare()
+        {
+            return Math.Pow(Side, 2);
+        }
+    }
+}
